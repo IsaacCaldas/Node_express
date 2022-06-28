ab9ffd753fac8611e83d3eb686635df257d5fb19
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-28 15:36:34 -0300

    Route index

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const APP = express()
+const app = express()
 const PORT = 3000
 
-APP.listen(PORT, () => {
+app.use('/', (req, res) => {
+  res.send(`<h1>Server is running on port=<span style="color: #22a176">${PORT}</span></h1>`)
+})
+
+app.listen(PORT, () => {
   console.log(`Server is running on port=${PORT}`)
 })
