7b15964ae71150b0ac45bc772c38cb5e93251d38
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-28 15:14:30 -0300

    Nodemon

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello world!')
+const express = require('express')
+const APP = express()
+const PORT = 3000
+
+APP.listen(PORT, () => {
+  console.log(`Server is running on port=${PORT}`)
+})
