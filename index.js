9ca7c526a09bd4f4082b23a1b4a06615a05a10d3
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-28 15:43:07 -0300

    Responses formats

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const app = express()
 const PORT = 3000
 
 app.use('/', (req, res) => {
-  res.send(`<h1>Server is running on port=<span style="color: #22a176">${PORT}</span></h1>`)
+  res.json({
+    online_users: [
+      { id: 0, name: 'Anonymous', admin: true },
+      { id: 1, name: 'John Doe', admin: true },
+      { id: 2, name: 'Fulano', admin: false },
+      { id: 3, name: 'Beltrano', admin: false },
+      { id: 3, name: 'Ciclano', admin: false },
+    ]
+  })
+  // res.json({
+  //   name: 'Anonymous',
+  //   admin: true
+  // })
+  // res.send(`Server is running on port=<span style="color: #22a176">${PORT}</span>`)
 })
 
 app.listen(PORT, () => {
